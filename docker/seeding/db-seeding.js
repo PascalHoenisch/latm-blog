5146865b765575a749ec3324c254a55fdd6c3b9e
PascalHoenisch <pascal@hoenisch.eu>
2023-12-22 16:12:39 -0300

    update seeder

--- docker/seeding/db-seeding.js
+++ docker/seeding/db-seeding.js
@@ -42,15 +42,26 @@ db.getCollection("blogs").insertOne({
 
 db.getCollection("authors").insertOne({
     alt: {
-        en: "john doe looking sweet at you with a hat",
-        es: "John Doe te mira dulcemente y tiene un sombrero puesto.",
-        de: "john doe schaut dich süß an und hat dabei einen hut auf"
+        en: 'john doe looking sweet at you with a hat',
+        es: 'John Doe te mira dulcemente y tiene un sombrero puesto.',
+        de: 'john doe schaut dich süß an und hat dabei einen hut auf'
     },
     about: {
-        en: "hello I am John doe <br> i like to travel",
-        es: "hola soy john doe, me gusta viajar",
-        de: "hallo ich bin John doe, <br> ich reise gerne"
+        en: 'hello I am John doe <br> i like to travel hello I am John doe <br> i like to travel hello I am John doe i like to travel hello I am John doe i like to travel hello I am John doe i like to travel hello I am John doe <br> i like to travel hello I am John doe <br> i like to travel hello I am John doe <br> i like to travel',
+        es: 'hola soy john doe, me gusta viajar',
+        de: 'hallo ich bin John doe, <br> ich reise gerne'
     },
-    imageUri: "/author/nice-image.jpeg",
-    name: "John Doe"
+    imageUri: 'author/nice-image.jpeg',
+    name: 'Pascal',
+    previewImageUri: 'author/preview/author.png',
+    slogan: {
+        en: 'english slogan',
+        es: 'espańol slogan',
+        de: 'deutscher slogan'
+    },
+    previewImageAlt: {
+        en: 'john doe looking sweet at you with a hat only the face',
+        es: 'John Doe te mira dulcemente y tiene un sombrero puesto.',
+        de: 'john doe schaut dich süß an und hat dabei einen hut auf'
+    }
 });
