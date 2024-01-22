8d9b7b5908d336fc223174bd734f105ec7d80ec9
PascalHoenisch <pascal@hoenisch.eu>
2024-01-22 11:45:45 -0300

    add todos for http error codes

--- app/src/routes/[[lang]]/+layout.server.js
+++ app/src/routes/[[lang]]/+layout.server.js
@@ -1,13 +1,19 @@
 import {locales} from "$lib/lang/i18n.js";
 import {IMAGE_SERVER_URL, DOMAIN} from "$env/static/private";
+// todo uncomment error check and fix issues
+//import {Error} from "svelte-check";
+import {error} from "@sveltejs/kit";
 
 // @ts-ignore
 export function load({params}) {
-    // @ts-ignore
     // set language fallback to german /de prefix
+    // @ts-ignore
     let language = locales[params.lang ?? 'de'];
     // check if locale exists and is not malformed
     if (!language || !Object.values(locales).includes(language)) {
+        // return a 404 Language not found svelte kit error code
+        error(404, {message: "Language not found" });
+        // todo add error template
         language = 'de';
     }
 
