32efccbc234cd214c30c8525e6360f3bf64c215a
PascalHoenisch <pascal@hoenisch.eu>
2024-01-22 11:26:57 -0300

    fix language not found issue set german as fallback and do not return an error code 500

--- app/src/routes/[[lang]]/+layout.server.js
+++ app/src/routes/[[lang]]/+layout.server.js
@@ -6,6 +6,11 @@ export function load({params}) {
     // @ts-ignore
     // set language fallback to german /de prefix
     let language = locales[params.lang ?? 'de'];
+    // check if locale exists and is not malformed
+    if (!language || !Object.values(locales).includes(language)) {
+        language = 'de';
+    }
+
     return {
         language: language,
         imageServerUrl: IMAGE_SERVER_URL,
